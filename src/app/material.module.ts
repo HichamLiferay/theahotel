fe21c874fcff3b20c2d45129cde43f24721326d0
Hicham NACIRI <mohammed.hicham.naciri@gmail.com>
2019-12-10 00:37:54 +0100

    commit mat-side-nav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
     imports: [MatBadgeModule,
-              MatButtonModule],
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule],
     exports: [MatBadgeModule,
-              MatButtonModule]
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule
+    ]
 })
 
 export class materialModule { }
